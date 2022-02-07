d69a9eec14e6d1b2e7294ef408be78990dcd6e5f
BODA MAHESH KUMAR <93734751+mahi-302@users.noreply.github.com>
2022-02-07 19:41:56 +0530

    Update jen.groovy

--- jen.groovy
+++ jen.groovy
@@ -4,10 +4,10 @@ pipeline{
        stage('hosting application'){
         steps{
           sh "ls"
-          sh "aws rds create-db-instance --db-instance-identifier test-mysql3 --db-name cwms --db-instance-class db.t2.micro --vpc-security-group-ids sg-03723ff32a5606bda --engine mysql --engine-version 5.7 --db-parameter-group-name default.mysql5.7 --publicly-accessible  --master-username admin --master-user-password mahesh8842 --allocated-storage 10 --region us-east-2"
+          sh "aws rds create-db-instance --db-instance-identifier test-mysql --db-name cwms --db-instance-class db.t2.micro --vpc-security-group-ids sg-03723ff32a5606bda --engine mysql --engine-version 5.7 --db-parameter-group-name default.mysql5.7 --publicly-accessible  --master-username admin --master-user-password mahesh8842 --allocated-storage 10 --region us-east-2"
           sleep(400)
           script{
-              def cmd = "aws rds describe-db-instances --db-instance-identifier test-mysql3 --region us-east-2"
+              def cmd = "aws rds describe-db-instances --db-instance-identifier test-mysql --region us-east-2"
               def output = sh(script: cmd,returnStdout: true)
               jsonitem = readJSON text: output
               println(jsonitem)
@@ -21,15 +21,15 @@ pipeline{
               sleep(100)
             }
           script{
-              def cmd = "aws elbv2 create-target-group --name my-targets3 --protocol HTTP --port 80 --target-type instance --vpc-id vpc-0a7387e5c61ec7456 --region us-east-2"
+              def cmd = "aws elbv2 create-target-group --name my-targets --protocol HTTP --port 80 --target-type instance --vpc-id vpc-0a7387e5c61ec7456 --region us-east-2"
               def output = sh(script: cmd,returnStdout: true)
               jsonitem2 = readJSON text: output
               println(jsonitem2)
               sleep(180)
                }
            sh "aws elbv2 create-listener --load-balancer-arn ${jsonitem1['LoadBalancers'][0]['LoadBalancerArn']} --protocol HTTP --port 80 --default-actions Type=forward,TargetGroupArn=${jsonitem2['TargetGroups'][0]['TargetGroupArn']} --region us-east-2"
-           sh "aws autoscaling create-launch-configuration --launch-configuration-name my-lc3-cli3 --image-id ami-0fb653ca2d3203ac1 --instance-type t2.micro --security-groups sg-03723ff32a5606bda --key-name Ec2all --iam-instance-profile role_all --user-data file://userdata.txt --region us-east-2"
-           sh "aws autoscaling create-auto-scaling-group --auto-scaling-group-name my-asg3-cli3 --launch-configuration-name my-lc3-cli3 --max-size 1 --min-size 1 --desired-capacity 1 --target-group-arns ${jsonitem2['TargetGroups'][0]['TargetGroupArn']} --availability-zones us-east-2c --region us-east-2"
+           sh "aws autoscaling create-launch-configuration --launch-configuration-name my-lc3-cli --image-id ami-0fb653ca2d3203ac1 --instance-type t2.micro --security-groups sg-03723ff32a5606bda --key-name Ec2all --iam-instance-profile role_all --user-data file://userdata.txt --region us-east-2"
+           sh "aws autoscaling create-auto-scaling-group --auto-scaling-group-name my-asg3-cli --launch-configuration-name my-lc3-cli --max-size 1 --min-size 1 --desired-capacity 1 --target-group-arns ${jsonitem2['TargetGroups'][0]['TargetGroupArn']} --availability-zones us-east-2c --region us-east-2"
         }
        }
   }
